286da3684991a07104143a2f9b86742ef2ece43d
test <localhost>
2021-12-05 03:41:31 -0800

    ?gitAlias.sh:"git subs X" runs "git X" in subdirs

--- gitAlias.sh
+++ gitAlias.sh
@@ -4,6 +4,6 @@ git cg init.defaultBranch 'main'
 git cg alias.uncg 'cg --unset'
 git cg alias.lol 'log --oneline --graph'
 git cg alias.undoc 'commit -c ORIG_HEAD'
-#git cg alias.undo 'reset HEAD~'
 git cg alias.undo '!git reset HEAD~ && echo "\"git undoc\" for git commit -c ORIG_HEAD" && :'
+git cg alias.subs '!x(){ find . -name ".git" -type d | sed s/\\/\.git//g | xargs -P4 -I{} git --git-dir={}/.git --work-tree={} $1;};x'
 
