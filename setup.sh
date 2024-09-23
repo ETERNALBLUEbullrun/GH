833b03af302833fd3b593fa3e73fd533c46ac2bc
TermuxFan <eternalbluebullrun@gmail.com>
2024-09-23 02:59:20 -0700

    ?setup.sh Print fingerprint's Sha256 hash
        to compare against GitHub, which now stores fingerprints as Sha256 hashes

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@ apt-get install vim ctags git
 . gitAlias.sh
 apt-get install openssh gh
 cp ~/../usr/etc/ssh/ssh_host_ed25519_key ~/.ssh/id_ed25519
+cp ~/../usr/etc/ssh/ssh_host_ed25519_key.pub ~/.ssh/id_ed25519.pub
+ssh-keygen -lf ~/.ssh/id_ed25519.pub -E sha256 #GitHub converts fingerprint to Sha256 hash
 cp ./gitinit ~/bin/
 gitinit
 
